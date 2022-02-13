4913fe075f6613d45560434168c4064f1fa76ea0
sumi.byun <selina2000@naver.com>
2022-02-14 04:50:16 +0900

    Feat : Recuit add ApplicationFormCreate Component and modify Button.tsx
    앞의 커밋에 수정내용이 실수로 같이 올라감.
    Button width, height 고정 풀어야 하지 않을까

--- src/components/organisms/Recruit.tsx
+++ src/components/organisms/Recruit.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import SectionTitle from '../atoms/SectionTitle';
+import SectionTitle from "../atoms/SectionTitle";
 function Recruit() {
-  return <div>
-    <SectionTitle mainTitle={"회원 모집"} subTitle={"새로운 회원을 모집해보세요. 😆"}/>
-
-
-  </div>;
+  return (
+    <div>
+      <SectionTitle
+        mainTitle={"회원 모집"}
+        subTitle={"새로운 회원을 모집해보세요. 😆"}
+      />
+    </div>
+  );
 }
 
 export default Recruit;
