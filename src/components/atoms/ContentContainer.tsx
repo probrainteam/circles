51366d9df394d9ee5b2a22720cec6aa36a134e60
sumi.byun <selina2000@naver.com>
2022-02-15 02:08:23 +0900

    fix : ContentContainer margin fix

--- src/components/atoms/ContentContainer.tsx
+++ src/components/atoms/ContentContainer.tsx
@@ -21,8 +21,6 @@ const ContentBoxWrapper = styled.div`
   position: relative;
   background: white;
   box-shadow: 4px 12px 30px 6px rgba(0, 0, 0, 0.09);
-  margin-top: 96px;
-  margin-bottom: 32px;
   display: flex;
   flex-direction: column;
 `;
